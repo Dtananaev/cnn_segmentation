a4a6b3a0e1bf0f2ab00359f60e690a4c941a45ae
Denis Tananaev <d.d.tananaev@gmail.com>
2021-12-14 09:51:16 +0100

    Update dataset_tutoriual.py

--- dataset_tutoriual.py
+++ dataset_tutoriual.py
@@ -19,33 +19,32 @@ SHUFFLE_BUFFER = 8
 
 def process_image(image_path, resize=True, data_type=tf.float32):
     """
-    Функция загрузки черно-белых картинок.
+    The function of the loading of the images
 
-    Входящие переменные:
-       image_path: название картинки с расположением в папке
-       (может быть входящая картинка или маска сегментации) 
-       (например: ./data/stage1_train/0a7d30b2523/images/0a7d30b252359a10fd298.png)
-    Выходящие переменные:
-       img: декодированная картинка
+    Args:
+       image_path: path to the image folder (can be image or segmentation mask)
+       (e.g.: ./data/stage1_train/0a7d30b2523/images/0a7d30b252359a10fd298.png)
+    Return:
+       img: decoded image
 
     """
-    # Считывание файла
+    # Read the file
     img = tf.io.read_file(image_path)
-    # Декодируем png формат
+    # Decode from png
     img = tf.image.decode_png(img, channels=IMG_CHANNELS) # grayscale
-    # Изменяем размер картинки в ширина=IMG_WIDTH и высота=IMG_HEIGHT
+    # Resize width=IMG_WIDTH and height=IMG_HEIGHT
     if resize:
          img = tf.expand_dims(img, axis=0)
          img = tf.compat.v1.image.resize_nearest_neighbor(img, [IMG_WIDTH, IMG_HEIGHT])
          img = tf.squeeze(img, axis=0)
-    # Конвертируем картинку из uint8 в float32 формат или bool (для маски)
+    # Convert to float32 for image or bool for binary mask
     img = tf.cast(img, data_type)
     return img
 
 
 def load_image_mask(image_path, mask_path):
     image = process_image(image_path, data_type=tf.float32)
-    image = image/ 255.0 # Нормализуем картинку до значений между 0 и 1
+    image = image/ 255.0 # normalization between 0 and 1
     mask = process_image(mask_path, data_type=tf.bool)
     mask = tf.cast(mask, tf.float32)
     return image, mask 
@@ -53,20 +52,20 @@ def load_image_mask(image_path, mask_path):
 
 def load_data(dataset_dir):
     """
-    Функция загружает картинки и маски сегментации.
-    Входящие переменные:
-    dataset_dir: директория с данными
+    The function loads images and segmentation binary masks.
+    Args:
+    dataset_dir: directory with data
     """
-    # Делаем список всех папок внутри папки dataset_dir
+    # Make a lists of the data in folders
     all_images_pathes = sorted(glob.glob(dataset_dir + "/*/images/*.png"))
     all_masks_pathes = sorted(glob.glob(dataset_dir + "/*/combined_mask/*.png"))
     dataset_size = int(len(all_images_pathes) / BATCH_SIZE)
 
     ds = tf.data.Dataset.from_tensor_slices((all_images_pathes, all_masks_pathes))
     ds = ds.map(load_image_mask, num_parallel_calls=2)
-    ds = ds.shuffle(buffer_size=SHUFFLE_BUFFER) # перемешивает картинки в случайном порядке (SHUFFLE_BUFFER -количество перемешиваемых картинок за раз)
-    ds = ds.batch(BATCH_SIZE) # размер батча (количество картинок которые загружаются в нейронку за раз)
-    ds = ds.prefetch(buffer_size=2) # подгружает в память 2 картинки что бы тренировка шла быстрее
+    ds = ds.shuffle(buffer_size=SHUFFLE_BUFFER) # randomly shuffle during training (SHUFFLE_BUFFER -number of shuffled images per time)
+    ds = ds.batch(BATCH_SIZE) # batch size (number of images which loaded to the CNN)
+    ds = ds.prefetch(buffer_size=2) # prefetch in RAM 2 images to make loading to GPU faster
     return ds, dataset_size
 
 if __name__ =="__main__":
